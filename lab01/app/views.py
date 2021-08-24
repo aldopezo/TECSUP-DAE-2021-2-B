2c04dde4ad73b3a6694e2bab0e80b527b8e1125f
Aldo Pezo <aldo.pezo.m@gmail.com>
2021-08-24 01:37:12 -0500

    Lab01

---
+++ lab01/app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Respuestas")
+
+def sumar(request, numero1, numero2):
+    return HttpResponse("La suma es: " 
+    + str(numero1 + numero2))
+
+def restar(request, numero1, numero2):
+    return HttpResponse("La resta es: " 
+    + str(numero1 - numero2))
+
+def multiplicar(request, numero1, numero2):
+    return HttpResponse("La multiplicacion es: " 
+    + str(numero1 * numero2))
+
+def dividir(request, numero1, numero2):
+    return HttpResponse("La division es: " 
+    + str(numero1 / numero2))
