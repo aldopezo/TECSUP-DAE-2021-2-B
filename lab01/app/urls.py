9734cbf95606800c1f6319136b942d4f2ea5c3ae
aldopezo <67888765+aldopezo@users.noreply.github.com>
2021-08-24 01:43:35 -0500

    Update urls.py

--- lab01/app/urls.py
+++ lab01/app/urls.py
@@ -3,17 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-
-    path('sumar/<int:numero1>/<int:numero2>/', views.sumar, name='sumar')
-]
-
-
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
     path('', views.index, name="index"),
 
     path('suma/<int:numero1>/<int:numero2>/', views.sumar, name='sumar'),
